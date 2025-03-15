7e1f3073c48996575e15b4a95bfc96de3d712844
trh4ckn0n <trhacknon@proton.me>
2025-03-15 23:03:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ app.register_blueprint(fav_route)
 app.register_blueprint(ipinfo_route)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
