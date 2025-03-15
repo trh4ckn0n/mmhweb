63624b5f33aa83afd6e3db27a1439db1e9909512
trh4ckn0n <trhacknon@proton.me>
2025-03-15 22:51:41 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_cors import CORS
+from models import db, init_db
+from routes.favicon_routes import fav_route
+from routes.ipinfo_routes import ipinfo_route
+
+app = Flask(__name__)
+
+# Configuration SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/favhash.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialisation de la DB
+db.init_app(app)
+init_db(app)
+
+# CORS
+CORS(app)
+
+# Enregistrement des routes
+app.register_blueprint(fav_route)
+app.register_blueprint(ipinfo_route)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
