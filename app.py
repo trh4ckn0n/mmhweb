3073538b4acfbb855de27df90b44c650d082a21c
trh4ckn0n <trhacknon@proton.me>
2025-03-15 23:48:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask_cors import CORS
 from models import db, init_db
 from routes.favicon_routes import fav_route
 from routes.ipinfo_routes import ipinfo_route
+from routes.favinfo_routes import fav_route  # Ajouter l'import de favinfo_route
+
 import os
 
 app = Flask(__name__)
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'database/favhash.db')}"
-# Configuration SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialisation de la DB
@@ -27,6 +27,9 @@ CORS(app)
 # Enregistrement des routes
 app.register_blueprint(fav_route)
 app.register_blueprint(ipinfo_route)
+
+# Ajout du blueprint favinfo_route
+app.register_blueprint(fav_route)  # Assure-toi d'ajouter celui-ci
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=5000)
